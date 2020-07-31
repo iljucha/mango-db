e4448d1952d14ada374693720c8c05a58c00a28f
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-07-31 11:51:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 import MangoDB from "./lib/mango-db.js"
 import Cursor from "./lib/cursor.js"
 import * as Types from "./lib/types.js"
-export { Cursor, Types }
+export { MangoDB, Cursor, Types }
 export default MangoDB
