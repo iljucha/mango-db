6a3c73180f52dc414e5be41afafb48aeb71ba787
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-08-05 12:32:57 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// @ts-check
-import MangoDB from "./lib/mango-db.js"
-import Cursor from "./lib/cursor.js"
-import * as Types from "./lib/types.js"
-export { MangoDB, Cursor, Types }
-export default MangoDB
